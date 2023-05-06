2637c4f08ce3eb2b7691fba8fe656d7ae1ba256a
Alex S <alex.savchenko@ambyint.com>
2023-05-06 12:33:01 -0600

    formating

--- main-test.py
+++ main-test.py
@@ -4,7 +4,7 @@ from main import Add
 
 def TestAdd():
     assert Add(2, 3) == 5
-    print("Add Function works correctly")
+    print("Add Function works correctly!")
 
 
 if __name__ == '__main__':
